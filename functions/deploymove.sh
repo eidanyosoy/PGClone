991ae7bf68dd8954e353a4b2bb1b7e7ecd30914d
doob187 <60312740+doob187@users.noreply.github.com>
2021-02-14 12:59:54 +0100

    Update deploymove.sh

--- functions/deploymove.sh
+++ functions/deploymove.sh
@@ -14,28 +14,26 @@ executemove() {
     rm -rf plexguide/deployed.version
     # Call Variables
     pgclonevars
-	# update system to new packages
-    ansible-playbook /opt/pgclone/ymls/update.yml
+   # update system to new packages
+    ansible-playbook /opt/pgclone/ymls/update.yml 1>/dev/null 2>&1
     # flush and clear service logs
     cleanlogs
     # to remove all service running prior to ensure a clean launch
-    ansible-playbook /opt/pgclone/ymls/remove.yml
+    ansible-playbook /opt/pgclone/ymls/remove.yml 1>/dev/null 2>&1
     # gdrive deploys by standard
     echo "gdrive" >/var/plexguide/deploy.version
     echo "mu" >/var/plexguide/deployed.version
     type=gdrive
-    ansible-playbook /opt/pgclone/ymls/drive.yml -e "drive=gdrive"
+    ansible-playbook /opt/pgclone/ymls/drive.yml -e "drive=gdrive" 1>/dev/null 2>&1
     # deploy only if pgmove is using encryption
     if [[ "$transport" == "me" ]]; then
         echo "me" >/var/plexguide/deployed.version
         type=gcrypt
-        ansible-playbook /opt/pgclone/ymls/drive.yml -e "drive=gcrypt"
+        ansible-playbook /opt/pgclone/ymls/drive.yml -e "drive=gcrypt" 1>/dev/null 2>&1
     fi
     # output final display
     if [[ "$type" == "gdrive" ]]; then
         finaldeployoutput="Move - Unencrypted"
     else finaldeployoutput="Move - Encrypted"; fi
-    deploydrives
-
-	
+    deploydrives	
 }
