72750b5843ab3836faac03a2356d5d8583aa1708
doob187 <60312740+doob187@users.noreply.github.com>
2021-02-15 19:34:41 +0100

    Update variables.sh

--- functions/variables.sh
+++ functions/variables.sh
@@ -12,37 +12,15 @@ pgclonevars() {
 		if [ ! -e "$file" ]; then echo "$2" >$1; fi
 		}
 
-    touch /var/plexguide/uagent
-    uagentrandom="$(cat /var/plexguide/uagent)"
-    if [[ "$uagentrandom" == "NON-SET" || "$uagentrandom" == "" || "$uagentrandom" == "rclone/v1.*" || "$uagentrandom" == "random" || "$uagentrandom" == "RANDOM" ]]; then
-		randomagent=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
-		uagent=$(cat /var/plexguide/uagent)
-		echo "$randomagent" >/var/plexguide/uagent
-		echo $(sed -e 's/^"//' -e 's/"$//' <<<$(cat /var/plexguide/uagent)) >/var/plexguide/uagent; fi
-
-	touch /var/plexguide/cloneclean.nzb
-	touch /var/plexguide/cloneclean.torrent
-	cleanernzb="$(cat /var/plexguide/cloneclean.nzb)"
-	cleanertorrenet="$(cat /var/plexguide/cloneclean.torrent)"
-	if [[ "$cleanernzb" == "600" || "$cleanernzb" == "" || "$cleanernzb" == "NON-SET" ]]; then echo "600" >/var/plexguide/cloneclean.nzb; fi
-	if [[ "$cleanertorrenet" == "2400" || "$cleanertorrenet" == "" || "$cleanertorrenet" == "NON-SET" ]]; then echo "2400" >/var/plexguide/cloneclean.torrent; fi
-
     # rest standard
     mkdir -p /var/plexguide/rclone
     variable /var/plexguide/project.account "NOT-SET"
     variable /var/plexguide/rclone/deploy.version "null"
     variable /var/plexguide/pgclone.transport "NOT-SET"
-    variable /var/plexguide/move.bw "9M"
-    # variable /var/plexguide/blitz.bw "1000M"
     variable /var/plexguide/pgclone.salt ""
     variable /var/plexguide/multihd.paths ""
     variable /var/plexguide/server.hd.path "/mnt"
     hdpath="$(cat /var/plexguide/server.hd.path)"
-
-    gce="/var/plexguide/gce.check"
-    if [[ ! -e $gce ]]; then
-       variable /var/plexguide/blitz.bw "1000M"
-    else variable /var/plexguide/blitz.bw "4500M"; fi
 
     variable /var/plexguide/oauth.check ""
     oauthcheck=$(cat /var/plexguide/oauth.check)
@@ -117,85 +95,9 @@ pgclonevars() {
     variable /var/plexguide/.tmp.multihd
     multihds=$(cat /var/plexguide/.tmp.multihd)
 
-    if [[ "$dversion" == "mu" ]]; then
-        dversionoutput="Move"
-    elif [[ "$dversion" == "me" ]]; then
-        dversionoutput="Move: Encrypted"
-    elif [[ "$dversion" == "bu" ]]; then
-        dversionoutput="Blitz"
+   If [[ "$dversion" == "bu" ]]; then
+        dversionoutput="TDRIVE"
     elif [[ "$dversion" == "be" ]]; then
-        dversionoutput="Blitz: Encrypted"
-    elif [[ "$dversion" == "le" ]]; then
-        dversionoutput="Local"
+        dversionoutput="TDRIVE: Encrypted"
     else dversionoutput="None"; fi
-
-    # # For Clone Clean
-    # if [[ ! -e $gce ]]; then
-       # variable /var/plexguide/cloneclean "600"
-    # else variable /var/plexguide/cloneclean "120"; fi
-
-    variable /var/plexguide/vfs_bs "16M"
-    vfs_bs=$(cat /var/plexguide/vfs_bs)
-
-    variable /var/plexguide/vfs_dcs "64M"
-    vfs_dcs=$(cat /var/plexguide/vfs_dcs)
-
-    variable /var/plexguide/vfs_dct "5m"
-    vfs_dct=$(cat /var/plexguide/vfs_dct)
-
-    variable /var/plexguide/vfs_cm "writes"
-    vfs_cm=$(cat /var/plexguide/vfs_cm)
-
-    variable /var/plexguide/vfs_cma "1h"
-    vfs_cma=$(cat /var/plexguide/vfs_cma)
-
-    variable /var/plexguide/vfs_cms "10G"
-    vfs_cms=$(cat /var/plexguide/vfs_cms)
-
-    variable /var/plexguide/vfs_rcs "64M"
-    vfs_rcs=$(cat /var/plexguide/vfs_rcs)
-
-    variable /var/plexguide/vfs_rcsl "2048M"
-    vfs_rcsl=$(cat /var/plexguide/vfs_rcsl)
-
-    variable /var/plexguide/vfs_ll "ERROR"
-    vfs_ll=$(cat /var/plexguide/vfs_ll)
-
-    if [[ ! -e $gce ]]; then
-       variable /var/plexguide/vfs_t "8"
-    else variable /var/plexguide/vfs_t "16"; fi
-    vfs_t=$(cat /var/plexguide/vfs_t)
-
-    if [[ ! -e $gce ]]; then
-       variable /var/plexguide/vfs_c "16"
-    else variable /var/plexguide/vfs_c "32"; fi
-    vfs_c=$(cat /var/plexguide/vfs_c)
-
-    if [[ ! -e $gce ]]; then    
-       variable /var/plexguide/vfs_mt "350G"
-    else variable /var/plexguide/vfs_mt "720G"; fi
-    vfs_mt=$(cat /var/plexguide/vfs_mt)
-
-    if [[ ! -e $gce ]]; then
-       variable /var/plexguide/vfs_test "4G"
-    else variable /var/plexguide/vfs_test "16G"; fi
-    vfs_test=$(cat /var/plexguide/vfs_test)
-
-    # For BWLimit
-    if [[ ! -e $gce ]]; then
-       variable /var/plexguide/timetable.bw "14:00,40M"
-    else variable /var/plexguide/timetable.bw "off"; fi
-
-    # Upgrade old var format to new var format
-
-    echo $(sed -e 's/^"//' -e 's/"$//' <<<$(cat /var/plexguide/uagent)) >/var/plexguide/uagent
-    if [[ $(cat /var/plexguide/blitz.bw) != *"M"* && $(cat /var/plexguide/blitz.bw) != 0 ]]; then echo "$(cat /var/plexguide/blitz.bw)M" >/var/plexguide/blitz.bw; fi
-    if [[ $(cat /var/plexguide/move.bw) != *"M"* && $(cat /var/plexguide/move.bw) != 0 ]]; then echo "$(cat /var/plexguide/move.bw)M" >/var/plexguide/move.bw; fi
-    if [[ $(cat /var/plexguide/vfs_bs) != *"M" ]]; then echo "$(cat /var/plexguide/vfs_bs)M" >/var/plexguide/vfs_bs; fi
-    if [[ $(cat /var/plexguide/vfs_dcs) != *"M" ]]; then echo "$(cat /var/plexguide/vfs_dcs)M" >/var/plexguide/vfs_dcs; fi
-    if [[ $(cat /var/plexguide/vfs_dct) != *"m" ]]; then echo "$(cat /var/plexguide/vfs_dct)m" >/var/plexguide/vfs_dct; fi
-    if [[ $(cat /var/plexguide/vfs_cma) != *"h" ]]; then echo "1h" >/var/plexguide/vfs_cma; fi
-    if [[ $(cat /var/plexguide/vfs_cms) != *"G" && $(cat /var/plexguide/vfs_cms) != "off" ]]; then echo "off" >/var/plexguide/vfs_cms; fi
-    if [[ $(cat /var/plexguide/vfs_rcs) != *"M" ]]; then echo "$(cat /var/plexguide/vfs_rcs)M" >/var/plexguide/vfs_rcs; fi
-    if [[ $(cat /var/plexguide/vfs_rcsl) != *"M" && $(cat /var/plexguide/vfs_rcsl) != "off" ]]; then echo "2048M" >/var/plexguide/vfs_rcsl; fi
 }
